074eb9fbe6d9eb83d76b078ad87937bbe6fda763
Joshua Egan <josh-egan@pluralsight.com>
2015-12-09 14:20:03 -0700

    day 1 challenges

---
+++ challenges/day1/elevator.js
@@ -0,0 +1,26 @@
+function matchCount(s, regex) {
+  let matches = s.match(regex)
+  if (matches === null) return 0
+  else return matches.length
+}
+export default {
+  getFloor(input) {
+    let up = matchCount(input, /\(/g)
+    let down = matchCount(input, /\)/g)
+    return up - down
+  },
+
+  getBasementEntryPosition(input) {
+    let s = input.slice(0)
+    let floor = 0
+    let idx = 0
+    while (s.length > 0 && floor >= 0) {
+      idx++
+      let movement = s[0] === '(' ? 1 : -1
+      floor += movement
+      s = s.slice(1)
+    }
+    if (floor === -1) return idx
+    throw new Error('You never entered the basement!')
+  }
+}
