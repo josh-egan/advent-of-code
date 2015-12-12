4448ac19087727d53ebcc7c026ce69f44d71d6b0
Josh Egan <josh.egan145@gmail.com>
2015-12-11 20:20:48 -0700

    refactor lights.js

--- challenges/day6/lights.js
+++ challenges/day6/lights.js
@@ -1,79 +1,11 @@
 import _ from 'lodash'
 
-function createGrid(size) {
-  let grid = []
-  for (var x = 0; x < size; x++) {
-    grid[x] = []
-    for (var y = 0; y < size; y++) {
-      grid[x][y] = 0
-    }
-  }
-  return grid
-}
-
-function parseInstruction(instruction, transforms) {
-  let fun
-  let parsedInstruction = instruction.slice(0)
-  if (_.startsWith(parsedInstruction, 'turn on')) {
-    parsedInstruction = parsedInstruction.replace('turn on', '')
-    fun = transforms.on
-  }
-  else if (_.startsWith(parsedInstruction, 'turn off')) {
-    parsedInstruction = parsedInstruction.replace('turn off', '')
-    fun = transforms.off
-  }
-  else if (_.startsWith(parsedInstruction, 'toggle')) {
-    parsedInstruction = parsedInstruction.replace('toggle', '')
-    fun = transforms.toggle
-  }
-  else {
-    throw new Error('unexpected instruction! ', instruction)
-  }
-
-  let coordinates = parsedInstruction.split('through')
-  let topLeftCoordinates = coordinates[0].split(',')
-  let bottomLeftCoordinates = coordinates[1].split(',')
-
-  let topLeft = {
-    x: Number.parseInt(topLeftCoordinates[0].trim()),
-    y: Number.parseInt(topLeftCoordinates[1].trim())
-  }
-
-  let bottomRight = {
-    x: Number.parseInt(bottomLeftCoordinates[0].trim()),
-    y: Number.parseInt(bottomLeftCoordinates[1].trim())
-  }
-
-  return (grid) => {
-    operateOnGrid(grid, topLeft, bottomRight, fun)
-  }
-}
-
-function operateOnGrid(grid, topLeft, bottomRight, transform) {
-  for (var x = topLeft.x; x <= bottomRight.x; x++) {
-    for (var y = topLeft.y; y <= bottomRight.y; y++) {
-      grid[x][y] = transform(grid[x][y])
-    }
-  }
-}
-
-function getFinalLightGrid(instructions, transforms) {
-  let lightsGrid = createGrid(1000)
-
-  instructions.forEach(instruction => {
-    let parsedOperation = parseInstruction(instruction, transforms)
-    parsedOperation(lightsGrid)
-  })
-
-  return lightsGrid
-}
-
 export default {
   countLitLights(instructions) {
     let transforms = {
       on: () => 1,
       off: () => 0,
-      toggle: (b) => b == 0 ? 1 : 0
+      toggle: b => b == 0 ? 1 : 0
     }
 
     let lightsGrid = getFinalLightGrid(instructions, transforms)
@@ -84,9 +16,9 @@ export default {
 
   countTotalBrightness(instructions) {
     let transforms = {
-      on: (b) => b + 1,
-      off: (b) => b > 0 ? b - 1 : 0,
-      toggle: (b) => b + 2
+      on: b => b + 1,
+      off: b => b > 0 ? b - 1 : 0,
+      toggle: b => b + 2
     }
 
     let lightsGrid = getFinalLightGrid(instructions, transforms)
@@ -94,7 +26,73 @@ export default {
     let totalBrightness = _.flatten(lightsGrid).reduce((prev, cur) => {
       return prev + cur
     }, 0)
-    
+
     return totalBrightness
+  }
+}
+
+function createGrid() {
+  let grid = []
+  for (var x = 0; x < 1000; x++) {
+    grid[x] = []
+    for (var y = 0; y < 1000; y++) {
+      grid[x][y] = 0
+    }
+  }
+  return grid
+}
+
+function getFinalLightGrid(instructions, transforms) {
+  let lightsGrid = createGrid()
+
+  instructions.forEach(instruction => {
+    let parsedOperation = parseInstruction(instruction, transforms)
+    parsedOperation(lightsGrid)
+  })
+
+  return lightsGrid
+}
+
+function parseInstruction(instruction, transforms) {
+  let transform
+  let parsedInstruction = instruction.slice(0)
+  if (_.startsWith(parsedInstruction, 'turn on')) {
+    parsedInstruction = parsedInstruction.replace('turn on', '')
+    transform = transforms.on
+  }
+  else if (_.startsWith(parsedInstruction, 'turn off')) {
+    parsedInstruction = parsedInstruction.replace('turn off', '')
+    transform = transforms.off
+  }
+  else if (_.startsWith(parsedInstruction, 'toggle')) {
+    parsedInstruction = parsedInstruction.replace('toggle', '')
+    transform = transforms.toggle
+  }
+  else {
+    throw new Error('unexpected instruction! ', instruction)
+  }
+
+  let coordinates = parsedInstruction.split('through')
+  let topLeftCoordinates = coordinates[0].split(',')
+  let bottomRightCoordinates = coordinates[1].split(',')
+
+  let topLeft = {
+    x: Number.parseInt(topLeftCoordinates[0].trim()),
+    y: Number.parseInt(topLeftCoordinates[1].trim())
+  }
+
+  let bottomRight = {
+    x: Number.parseInt(bottomRightCoordinates[0].trim()),
+    y: Number.parseInt(bottomRightCoordinates[1].trim())
+  }
+
+  return grid => operateOnGrid(grid, topLeft, bottomRight, transform)
+}
+
+function operateOnGrid(grid, topLeft, bottomRight, transform) {
+  for (var x = topLeft.x; x <= bottomRight.x; x++) {
+    for (var y = topLeft.y; y <= bottomRight.y; y++) {
+      grid[x][y] = transform(grid[x][y])
+    }
   }
 }
